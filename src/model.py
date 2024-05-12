c45d79a35e0a31d497620936af26632a9d9c3cd4
FungXF <fungxuefeng@gmail.com>
2024-05-12 15:07:03 +0800

    Added: bitsandbytes config

--- src/model.py
+++ src/model.py
@@ -5,7 +5,7 @@ import torch
 import asyncio
 
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, TextIteratorStreamer, TextStreamer
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, TextIteratorStreamer, BitsAndBytesConfig
 from langchain_core.prompts import PromptTemplate
 
 from custom_huggingface_pipeline import CustomHuggingFacePipeline
@@ -36,33 +36,36 @@ class Inference:
         """
         Loads the model for the inference pipeline.
 
-        This function initializes the model for the inference pipeline based on the provided model ID. 
-        The model is loaded using the `AutoModelForCausalLM.from_pretrained` method from the Hugging Face Transformers library. 
+        This method initializes the model for the inference pipeline based on the provided model ID.
+        The model is loaded using the `AutoModelForCausalLM.from_pretrained` method from the Hugging Face Transformers library.
         The model is loaded with the following parameters:
-        
-        - `model_id` (str): The ID of the pretrained model to load.
-        - `cache_dir` (str, optional): The directory to cache the pretrained model. Defaults to the value of the `HF_CACHE` environment variable.
-        - `load_in_4bit` (bool, optional): Whether to load the model in 4-bit quantization. Defaults to `True`.
-        - `device_map` (str or dict, optional): The device map specifying the devices to use for computation. Defaults to `"auto"`.
-        - `torch_dtype` (torch.dtype, optional): The data type to use for computation. Defaults to `torch.bfloat16`.
-        - `token` (str, optional): The token to use for authentication. Defaults to the value of the `HF_TOKEN` environment variable.
-        
-        The loaded model is then assigned to the `model` attribute of the current object.
+
+        - model_id (str): The ID of the pretrained model to load.
+        - cache_dir (str, optional): The directory to cache the pretrained model. Defaults to the value of the `HF_CACHE` environment variable.
+        - load_in_4bit (bool, optional): Whether to load the model in 4-bit quantization. Defaults to `True`.
+        - device_map (str or dict, optional): The device map specifying the devices to use for computation. Defaults to `"auto"`.
+        - torch_dtype (torch.dtype, optional): The data type to use for computation. Defaults to `torch.bfloat16`.
+        - token (str, optional): The token to use for authentication. Defaults to the value of the `HF_TOKEN` environment variable.
 
         Parameters:
             self (Inference): The current instance of the Inference class.
-        
+
         Returns:
             None
         """
+        nf4_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_compute_dtype=torch.bfloat16
+            )
         model_id = self.params["model_id"]
         self.model = AutoModelForCausalLM.from_pretrained(
             model_id,
             cache_dir=os.environ.get("HF_CACHE"),
-            load_in_4bit=True,
             device_map="auto",
-            torch_dtype=torch.bfloat16,
-            token=os.environ.get("HF_TOKEN")
+            token=os.environ.get("HF_TOKEN"),
+            quantization_config=nf4_config
             )
         
     def streaming(self) -> TextIteratorStreamer:
